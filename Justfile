89538591b4f120a3d7c957ad15730c4e6f0e5b66
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-09-19 15:08:38 +0200

    refactor: Switch to generating WIT bindings in `build.rs`

--- Justfile
+++ Justfile
@@ -1,11 +1,7 @@
 gen-obelisk-ext:
 	scripts/obelisk-generate-extensions.sh
 
-gen-wit-bindgen:
-	(cd workflow/deployer-workflow/impl-flyio && wit-bindgen rust --generate-all --out-dir ./src/generated wit)
-
-
-build: gen-wit-bindgen
+build:
 	(cd workflow/deployer-workflow/impl-flyio && cargo build --release)
 
 serve:
---
+++ workflow/deployer-workflow/impl-flyio/build.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use std::path::Path;
+use wit_bindgen_rust::Opts;
+use wit_parser::Resolve;
+
+fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=wit/");
+
+    let mut opts = Opts::default();
+    opts.generate_all = true;
+    let mut generator = opts.build();
+    let mut resolve = Resolve::default();
+    let (pkg, _files) = resolve.push_path("wit")?;
+    let main_packages = vec![pkg];
+    let world = resolve.select_world(&main_packages, None)?;
+    let mut files = Default::default();
+    generator.generate(&resolve, world, &mut files)?;
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let dst = Path::new(&out_dir).join("generated.rs");
+    let (_name, contents) = files.iter().next().unwrap();
+    std::fs::write(&dst, contents)?;
+    Ok(())
+}
