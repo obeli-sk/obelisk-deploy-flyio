a282e45ff36106a2876fa537a45700ddd1e99d17
Tomas Olvecky <tomas.olvecky@gmail.com>
2026-02-12 11:37:06 +0100

    build: Replace `sed` with `obelisk component add`, fix updating test snapshots

--- scripts/push-components.sh
+++ scripts/push-components.sh
@@ -10,23 +10,27 @@ TOML_FILE="obelisk-oci.toml"
 PREFIX="docker.io/getobelisk/components_flyio_"
 
 push() {
-    RELATIVE_PATH=$1
-    FILE_NAME_WITHOUT_EXT=$(basename "$RELATIVE_PATH" | sed 's/\.[^.]*$//')
-    OCI_LOCATION="${PREFIX}${FILE_NAME_WITHOUT_EXT}:${TAG}"
+    local COMPONENT_TYPE=$1
+    local RELATIVE_PATH=$2
+
+    local FILE_NAME_WITHOUT_EXT=$(basename "$RELATIVE_PATH" | sed 's/\.[^.]*$//')
+    local OCI_LOCATION="${PREFIX}${FILE_NAME_WITHOUT_EXT}:${TAG}"
     echo "Pushing ${RELATIVE_PATH} to ${OCI_LOCATION}..."
-    OUTPUT=$(obelisk component push "$RELATIVE_PATH" "$OCI_LOCATION")
+    local OUTPUT=$(obelisk component push "$RELATIVE_PATH" "$OCI_LOCATION")
 
     # Replace the old location with the actual OCI location
+
     sed -i -E "/name = \"${FILE_NAME_WITHOUT_EXT}\"/{n;s|location\.oci = \".*\"|location.oci = \"${OUTPUT}\"|}" "$TOML_FILE"
+    obelisk component add ${COMPONENT_TYPE} ${OUTPUT} --name ${FILE_NAME_WITHOUT_EXT} -c $TOML_FILE
 }
 
 # Build components
 just build
 
-push "target/wasm32-unknown-unknown/release_workflow/obelisk_deployer_flyio.wasm"
-push "target/wasm32-wasip2/release_webhook/webhook_healthcheck.wasm"
+push workflow "target/wasm32-unknown-unknown/release_workflow/obelisk_deployer_flyio.wasm"
+push webhook_endpoint "target/wasm32-wasip2/release_webhook/webhook_healthcheck.wasm"
 
 echo "All components pushed and TOML file updated successfully."
 
 # obelisk.toml is parsed in toml.rs, snapshots will need updating.
-INSTA_UPDATE=always ./scripts/test.sh
+INSTA_UPDATE=always just test
